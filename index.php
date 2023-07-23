310f2b2fa10cbc979a1c359ef3c0b4c004856acb
kk1169 <39980501+kk1169@users.noreply.github.com>
2023-07-23 12:43:33 +0530

    Added js files

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Employee Application</title>
 </head>
 
 <body>
-    Employee Home Page
+
 </body>
 
 </html>
